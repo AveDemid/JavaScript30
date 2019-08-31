3653d2fa480852fb947d06d1c3437b45aba8da67
Demid Borodin <ave.demid@gmail.com>
2019-08-31 12:01:10 +0300

    CSS Text Shadow Mouse Move Effect | Day 16

--- src/16-css-text-shadow-mouse-move-effect/index.js
+++ src/16-css-text-shadow-mouse-move-effect/index.js
@@ -1,1 +1,39 @@
 import "./main.css";
+
+const throttle = (fn, delay) => {  
+  let lastCall = 0;
+  return (...args) => {
+    const now = new Date().getTime();
+
+    if(now - lastCall > delay) {
+      lastCall = now;
+      return fn(...args)
+    }
+  }
+}
+
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 100;
+
+const moveShadow = (event) => {
+  const { offsetWidth: width, offsetHeight: height } = hero; 
+  let { offsetX: x, offsetY: y } = event;
+
+  if(event.target !== hero) {
+    x = x + text.offsetLeft;
+    y = y + text.offsetTop;
+
+    console.log(text.offsetTop);
+  }
+
+  console.log(x, y);
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 black`;
+  console.log(text);
+};
+
+hero.addEventListener("mousemove", throttle(moveShadow, 30));
