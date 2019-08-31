24e6a89cb0f09cd774a5db6555f61d92697dfe21
Demid Borodin <ave.demid@gmail.com>
2019-08-31 13:37:10 +0300

    Tally String Times with Reduce | Day 18

--- src/18-tally-string-times-with-reduce/index.js
+++ src/18-tally-string-times-with-reduce/index.js
@@ -1,1 +1,27 @@
 import "./main.css";
+
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [minutes, seconds] = timeCode.split(":").map(parseFloat);
+    
+    return minutes * 60 + seconds;
+  })
+  .reduce((previousValie, currentValue) => previousValie + currentValue)
+
+let secondsLeft = seconds;
+
+const  hoursInSeconds = (secondsLeft - (secondsLeft % 3600));
+secondsLeft -=  hoursInSeconds;
+
+const minutesInSeconds = (secondsLeft - (secondsLeft % 60));
+secondsLeft -= minutesInSeconds;
+
+const hours = hoursInSeconds / 3600;
+const minutes = minutesInSeconds / 60;
+
+const time = `${hours}:${minutes}:${secondsLeft}`
+
+console.log(time);
